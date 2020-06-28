42111a03493fc2a4746668f6412dd3a063914783
Isaias Quintanilla <quintanillaisaias@gmail.com>
2020-06-28 15:10:12 -0500

    created DB Setup and Config Setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
